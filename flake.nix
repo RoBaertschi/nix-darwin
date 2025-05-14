2f4f2ac8ccb529ff1a5c07975320c04006daf67d
RoBaertschi <rtmbaertschi007@gmail.com>
2025-05-14 08:41:53 +0200

    added zen browser

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         enable = true;
         taps = [];
         brews = [];
-        casks = ["iterm2" "proton-pass" "mac-mouse-fix"];
+        casks = ["iterm2" "proton-pass" "mac-mouse-fix" "zen"];
       };
 
       # Necessary for using flakes on this system.
